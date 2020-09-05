c2b70325d9c124ce8edd11003323f1a30af57248
akhil-sapaad <akhil@sapaad.com>
2020-09-05 16:46:32 +0530

    initial commit

---
+++ sapaad-task1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var first_name = "Akhil"
+	var last_name = "G Krishnan"
+	var odd_count = 0
+	var even_count = 0
+	for a := 1; a <= 100; a++ {
+		if a%2 == 0 {
+			fmt.Printf("Even :%s", first_name)
+			fmt.Printf("\n")
+			odd_count++
+		} else {
+			fmt.Printf("Odd :%s", last_name)
+			fmt.Printf("\n")
+			even_count++
+		}
+
+	}
+	fmt.Printf("%d :%d", odd_count, even_count)
+
+}
